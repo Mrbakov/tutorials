9a9233557c747c0d609903d27e09c8d64257bdb7
Martin Bakov <mrbakov@gmail.com>
2019-09-12 15:53:40 +0200

    Add laracasts laravel project

---
+++ Laravel/myfirstwebsite/app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = \App\Project::all();
+
+        return $projects;
+
+        return view('index');
+    }
+}
